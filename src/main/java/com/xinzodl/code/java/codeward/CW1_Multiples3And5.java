23ff5fe21e88eba7cb0ab78247ea6c052e4d8286
XinZodl <xinzodl@gmail.com>
2019-12-25 17:10:36 +0100

    Initial commit

---
+++ src/main/java/com/xinzodl/code/java/codeward/CW1_Multiples3And5.java
@@ -0,0 +1,13 @@
+package com.xinzodl.code.java.codeward;
+
+import java.util.stream.IntStream;
+
+public class CW1_Multiples3And5 {
+    public int solution(int number) {
+        IntStream intStream = IntStream.range(1, number);
+        IntStream filteredIntStream = intStream.filter( x -> x % 3 == 0 || x % 5 == 0);
+        int result = filteredIntStream.reduce(Integer::sum).orElse(-1);
+        return result;
+    }
+
+}
---
+++ src/test/com/xinzodl/code/java/codeward/CW1_Multiples3And5Test.java
@@ -0,0 +1,11 @@
+package com.xinzodl.code.java.codeward;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CW1_Multiples3And5Test {
+    @Test
+    public void test() {
+        assertEquals(23, new CW1_Multiples3And5().solution(10));
+    }
+}
